cec48e5085afb07e43f906f96ddf9b6f5cad385e
Armand <jurkowski.armand@gmail.com>
2026-04-14 00:06:30 +0200

    fix: listen on 0.0.0.0 for Render port detection

--- server/src/index.js
+++ server/src/index.js
@@ -6,7 +6,7 @@ import config from './config.js'
 async function start() {
   const app = await buildApp()
   try {
-    await app.listen({ port: config.port })
+    await app.listen({ port: config.port, host: '0.0.0.0' })
   } catch (err) {
     app.log.error(err)
     process.exit(1)
